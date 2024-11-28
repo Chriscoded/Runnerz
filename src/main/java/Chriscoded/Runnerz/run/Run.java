a1384e768c29c93ad0ccf5b9b94ea72df3420384
chriscoded <amaemechris@gmail.com>
2024-11-28 05:49:35 +0100

    Run CRUD

--- src/main/java/Chriscoded/Runnerz/run/Run.java
+++ src/main/java/Chriscoded/Runnerz/run/Run.java
@@ -1,12 +1,22 @@
 package Chriscoded.Runnerz.run;
 
 import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 
 public record Run(
         Integer id,
-        String Title,
+        @NotEmpty
+        String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location) {
+    public Run {
+        if(!completedOn.isAfter(startedOn)){
+            throw new IllegalArgumentException("Completed On must be after Statred On");
+        }
+    }
 }
+
--- src/main/java/Chriscoded/Runnerz/run/RunController.java
+++ src/main/java/Chriscoded/Runnerz/run/RunController.java
@@ -1,10 +1,7 @@
 package Chriscoded.Runnerz.run;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
@@ -30,9 +27,30 @@ public class RunController {
     Run finById(@PathVariable Integer id){
         Optional<Run> run = runRepository.findById(id);
         if(run.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new RunNotFoundException();
         }
         return run.get();
+    }
+
+    //Post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create (@RequestBody Run run){
+        runRepository.create(run);
+    }
+
+    //Put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id){
+        runRepository.update(run,id);
+    }
+
+    //Delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    void delete(@PathVariable Integer id){
+        runRepository.delete(id);
     }
 
 }
