1480b11292c0229654b7310ad7d896b4d7f6dfb0
chriscoded <amaemechris@gmail.com>
2024-12-01 04:36:11 +0100

    Incoporate in Memory H2 Database

--- src/main/java/Chriscoded/Runnerz/run/RunRepository.java
+++ src/main/java/Chriscoded/Runnerz/run/RunRepository.java
@@ -1,6 +1,9 @@
 package Chriscoded.Runnerz.run;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,48 +14,16 @@ import java.util.Optional;
 
 @Repository
 public class RunRepository {
+    private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
 
-    private List<Run> runs = new ArrayList<>();
-
-    List<Run> finalAll(){
-        return runs;
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    Optional<Run> findById(Integer id){
-        return runs.stream()
-        .filter(run-> run.id() == id)
-                .findFirst();
-    }
-
-    void create(Run run){
-        runs.add(run);
-    }
-
-    void update(Run run, Integer id){
-        Optional<Run> existingRun = findById(id);
-        if(existingRun.isPresent()){
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
-    }
-
-    void delete(Integer id){
-        runs.removeIf(run -> run.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init(){
-        runs.add( new Run(1,
-                "Monday Morning run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-                3,
-                Location.INDOOR));
-
-        runs.add( new Run(2,
-                "Wednessday Morning run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-                6,
-                Location.OUTDOOR));
+    public List<Run> findAll(){
+        return jdbcClient.sql("select * from run")
+                .query(Run.class)
+                .list();
     }
 }
